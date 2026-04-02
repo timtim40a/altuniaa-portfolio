b7b7e41ad787d3e36079cc4bfd355be84f20a920
Tymur Soroka <timtim40a@gmail.com>
2026-04-03 00:07:57 +0100

    Blogpost HTML

--- src/app/_components/blogpost/blogpost.tsx
+++ src/app/_components/blogpost/blogpost.tsx
@@ -1,8 +1,16 @@
 import styles from "./blogpost.module.css";
 
 export default function BlogPost() {
-  return (
-    <article className={styles.container}>
-    </article>
-  );
+    return (
+        <article className={styles.container}>
+            <img className={styles.coverImage} alt="Cover" />
+            <h2 className={styles.title}>Blog Post Title</h2>
+            <p className={styles.excerpt}>
+                This is a short excerpt of the blog post. It gives a brief
+                overview of the content to entice readers to click and read
+                more.
+            </p>
+            <span className={styles.publishedAt}>Published on: 2024-06-01</span>
+        </article>
+    );
 }
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,9 +1,11 @@
+import BlogPost from "../_components/blogpost/blogpost";
 import styles from "./blog.module.css";
 
 export default function Blog() {
-  return (
-    <div className={styles.container}>
-      <h1>Blog</h1>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <h1>Blog</h1>
+            <BlogPost />
+        </div>
+    );
 }
