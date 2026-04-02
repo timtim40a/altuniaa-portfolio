08ee4cf5d0cd8c66b27c288cfc69c6f76f935cbb
Tymur Soroka <timtim40a@gmail.com>
2026-04-02 18:04:09 +0100

    Make home hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,20 @@ import styles from "./page.module.css";
 export default function Home() {
     return (
         <div className={styles.page}>
-            <h1 className={styles.title}>Welcome to my portfolio!</h1>
             <h2 className={styles.subtitle}>Welcome to my portfolio!</h2>
-            <h3 className={styles.subtitle}>Welcome to my portfolio!</h3>
-            <h4 className={styles.subtitle}>Welcome to my portfolio!</h4>
-            <h5 className={styles.subtitle}>Welcome to my portfolio!</h5>
+            <aside className={styles.aside}>
+                <img
+                    src="/images/astronaut.jpg"
+                    alt="Astronaut"
+                    className={styles.avatar}
+                />
+            </aside>
+            <p className={styles.text}>
+                I'm a software developer with a passion for creating innovative
+                and efficient solutions. This portfolio showcases some of my
+                projects and skills. Feel free to explore and contact me if
+                you'd like to collaborate or learn more about my work.
+            </p>
         </div>
     );
 }
