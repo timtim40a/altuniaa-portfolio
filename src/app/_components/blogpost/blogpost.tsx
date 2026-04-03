588775091b9980d14e65863503bfc98ee3ca64f3
Tymur Soroka <timtim40a@gmail.com>
2026-04-03 13:28:40 +0100

    Blogpost initial styling

--- src/app/_components/blogpost/blogpost.tsx
+++ src/app/_components/blogpost/blogpost.tsx
@@ -4,13 +4,17 @@ export default function BlogPost() {
     return (
         <article className={styles.container}>
             <img className={styles.coverImage} alt="Cover" />
-            <h2 className={styles.title}>Blog Post Title</h2>
-            <p className={styles.excerpt}>
-                This is a short excerpt of the blog post. It gives a brief
-                overview of the content to entice readers to click and read
-                more.
-            </p>
-            <span className={styles.publishedAt}>Published on: 2024-06-01</span>
+            <div className={styles.textContent}>
+                <h2 className={styles.title}>Blog Post Title</h2>
+                <p className={styles.excerpt}>
+                    This is a short excerpt of the blog post. It gives a brief
+                    overview of the content to entice readers to click and read
+                    more.
+                </p>
+                <span className={styles.publishedAt}>
+                    Published on: 2024-06-01
+                </span>
+            </div>
         </article>
     );
 }
