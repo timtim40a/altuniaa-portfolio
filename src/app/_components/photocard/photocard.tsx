7b3d7f637465f9fcd4f332eec8d206cefe70358e
Tymur Soroka <timtim40a@gmail.com>
2026-04-11 15:47:24 +0100

    Fetch images from sanity

--- src/app/_components/photocard/photocard.tsx
+++ src/app/_components/photocard/photocard.tsx
@@ -1,27 +1,29 @@
 import Image from "next/image";
 import styles from "./photocard.module.css";
 
-function Photocard({
-    title,
-    location,
-    takenAt,
-    img1,
-    alt = title,
-    // expanded = false,
-}: {
+export type PhotocardProps = {
     title: string;
+    image: string;
+    alt?: string;
     location: string;
     takenAt: string;
-    img1: string;
-    alt?: string;
     // expanded?: boolean;
-}) {
+};
+
+function Photocard({
+    title,
+    image,
+    alt = title,
+    location,
+    takenAt,
+    // expanded = false,
+}: PhotocardProps) {
     return (
         <div className={styles.container}>
             <div className={styles.imageContainer}>
                 <Image
                     className={styles.image}
-                    src={img1}
+                    src={image}
                     alt={alt}
                     fill
                     sizes="20vw 20vw"
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,53 +1,60 @@
-import Photocard from "../_components/photocard/photocard";
+import { ALL_PHOTOS_QUERY } from "@/sanity/lib/queries";
+import Photocard, { PhotocardProps } from "../_components/photocard/photocard";
 import styles from "./gallery.module.css";
+import { client } from "@/sanity/client";
 
-export default function Gallery() {
+export default async function Gallery() {
+    const photos: PhotocardProps[] = await client.fetch(ALL_PHOTOS_QUERY);
+
     return (
         <div className={styles.container}>
             <h1>Gallery</h1>
             <div className={styles.galleryGrid}>
-                <Photocard
+                {photos.map((photo) => (
+                    <Photocard key={photo.title} {...photo} />
+                ))}
+                {/* <Photocard
                     title="Pianissimo"
                     location="Kharkiv, Ukraine"
                     takenAt="2023-01-01"
-                    img1="/images/test1.jpg"
+                    image="/images/test1.jpg"
                 />
                 <Photocard
                     title="Fortissimo"
                     location="Kharkiv, Ukraine"
                     takenAt="2023-02-01"
-                    img1="/images/test2.jpg"
+                    image="/images/test2.jpg"
                 />
                 <Photocard
                     title="Fortissimo"
                     location="Kharkiv, Ukraine"
                     takenAt="2020-09-27"
-                    img1="/images/test3.jpg"
+                    image="/images/test3.jpg"
                 />
                 <Photocard
                     title="Fortissimo"
                     location="Kharkiv, Ukraine"
                     takenAt="2020-09-27"
-                    img1="/images/test4.jpg"
+                    image="/images/test4.jpg"
                 />
                 <Photocard
                     title="Fortissimo"
                     location="Kharkiv, Ukraine"
                     takenAt="2020-09-27"
-                    img1="/images/test5.jpg"
+                    image="/images/test5.jpg"
                 />
                 <Photocard
                     title="Fortissimo"
                     location="Kharkiv, Ukraine"
                     takenAt="2020-09-27"
-                    img1="/images/test6.jpg"
+                    image="/images/test6.jpg"
                 />
                 <Photocard
                     title="Fortissimo"
                     location="Kharkiv, Ukraine"
                     takenAt="2020-09-27"
-                    img1="/images/test7.jpg"
-                />
+                    image="/images/test7.jpg"
+                /> */}
             </div>
         </div>
     );
