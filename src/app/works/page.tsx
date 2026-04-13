cdab7d59f07b210aef825784f4e99269f8cb3c43
Tymur Soroka <timtim40a@gmail.com>
2026-04-13 13:49:27 +0100

    Fetch works from the CMS

--- src/app/works/page.tsx
+++ src/app/works/page.tsx
@@ -1,9 +1,19 @@
+import { client } from "@/sanity/client";
+import Workcard, { WorkcardProps } from "../_components/workcard/workcard";
 import styles from "./works.module.css";
+import { ALL_WORKS_QUERY } from "@/sanity/lib/queries";
 
-export default function Works() {
-  return (
-    <div className={styles.container}>
-      <h1>Works</h1>
-    </div>
-  );
+export default async function Works() {
+    const works: WorkcardProps[] = await client.fetch(ALL_WORKS_QUERY);
+
+    return (
+        <div className={styles.container}>
+            <h1>Works</h1>
+            <div className={styles.galleryGrid}>
+                {works.map((work) => (
+                    <Workcard key={work.slug} {...work} />
+                ))}
+            </div>
+        </div>
+    );
 }
